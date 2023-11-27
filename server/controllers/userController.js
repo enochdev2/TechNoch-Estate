cd14092d1c6caeae822a8b3a191950422b52a698
enoch-promise <enochpromise.va@gmail.com>
2023-11-27 13:27:59 -0800

    ListingModel completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/userModel.js';
 import { errorHandler } from '../utils/error.js';
-// import Listing from '../models/listing.model.js';
+import Listing from '../models/listingModel.js';
 
 
 export const updateUser = async (req, res, next) => {
@@ -45,18 +45,18 @@ export const deleteUser = async (req, res, next) => {
   }
 };
 
-// export const getUserListings = async (req, res, next) => {
-//   if (req.user.id === req.params.id) {
-//     try {
-//       const listings = await Listing.find({ userRef: req.params.id });
-//       res.status(200).json(listings);
-//     } catch (error) {
-//       next(error);
-//     }
-//   } else {
-//     return next(errorHandler(401, 'You can only view your own listings!'));
-//   }
-// };
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
 
 export const getUser = async (req, res, next) => {
   try {
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { deleteUser, updateUser,  
-    // getUserListings, 
+import { deleteUser, updateUser,  getUserListings, 
     getUser} from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
@@ -9,7 +8,7 @@ const router = express.Router();
 
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-// router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/listings/:id', verifyToken, getUserListings)
 router.get('/:id', verifyToken, getUser)
 
 export default router;
