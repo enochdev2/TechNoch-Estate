e464664508e15a2c21f65ea29f6f9bd733dfa1f8
enoch-promise <enochpromise.va@gmail.com>
2023-11-30 11:14:54 -0800

    contact page done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,37 +5,37 @@ import {FaFacebook, FaInstagram, FaTwitter, FaPinterest} from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div className='w-full px-8 py-0 bg-[#f8f8f8]'>
-            <div className='text-center flex'>
-                <FaFacebook className='font-8 mx-8 my-12 text-[#5651e5] m-4' />
-                <FaInstagram className='font-8 mx-8 my-12 text-[#5651e5] m-4' />
-                <FaTwitter className='font-8 mx-8 my-12 text-[#5651e5] m-4' />
-                <FaPinterest className='font-8 mx-8 my-12 text-[#5651e5] m-4' />
+        <div className='w-full px-2 py-0 bg-[#f8f8f8]'>
+            <div className='text-center w-full justify-center flex'>
+                <FaFacebook className='text-2xl font-bold mx-4 my-12 text-[#5651e5] m-4' />
+                <FaInstagram className='text-2xl mx-4 my-12 text-[#5651e5] m-4' />
+                <FaTwitter className='font-bold text-2xl mx-4 my-12 text-[#5651e5] m-4' />
+                <FaPinterest className='font-bold text-2xl mx-4 my-12 text-[#5651e5] m-4' />
             </div>
-            <div className='grid grid-cols-4 md:grid-cols-2 md:gap-20 gap-5'>
+            <div className='grid grid-cols-4 md:grid-cols-4 md:gap-10 gap-3'>
                 <div className='m-auto'>
-                    <h3 className='mx-3 my-0 '>About</h3>
+                    <h3 className='mx-3 my-3 underline font-bold '>About</h3>
                     <p className='mx-2 my-0'>Company</p>
                     <p className='mx-2 my-0'>Details</p>
                     <p className='mx-2 my-0'>Planning</p>
                     <p className='mx-2 my-0'>About Us</p>
                 </div>
                 <div className='m-auto'>
-                    <h3>Company</h3>
+                    <h3 className='mx-3 my-3 underline font-bold '>Company</h3>
                     <p>Company</p>
                     <p>Details</p>
                     <p>Planning</p>
                     <p>About Us</p>
                 </div>
                 <div className='m-auto'>
-                    <h3>Legal</h3>
+                    <h3 className='mx-3 my-3 underline font-bold '>Legal</h3>
                     <p>Company</p>
                     <p>Details</p>
                     <p>Planning</p>
                     <p>About Us</p>
                 </div>
                 <div className='m-auto'>
-                    <h3>Information</h3>
+                    <h3 className='mx-3 my-3 underline font-bold '>Information</h3>
                     <p>Company</p>
                     <p>Details</p>
                     <p>Planning</p>
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../../components/ListingItem';
+import hero from '../../assets/hero-img.jpg'
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
@@ -52,7 +53,10 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+      <div className='h-96 w-full overflow-hidden mt-8'>
+        <img src={hero} alt="hero" className='h-full w-full' />
+      </div>
+      <div className='flex flex-col gap-6 pt-14 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
           <br />
